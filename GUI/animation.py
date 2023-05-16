ce020f917faa95610b22337fda64a1feb352fbe5
Jakub Bień <bieniu257@gmail.com>
2023-05-16 18:05:54 +0200

    Animacja w nowym wątku

--- GUI/animation.py
+++ GUI/animation.py
@@ -1,12 +1,19 @@
+import asyncio
 import time
 from PySide6.QtCore import QRunnable
 
 class Animation(QRunnable):
-      def __init__(self, MainWindow, iter, numOfIters):
+      def __init__(self, MainWindow, iter, numOfIters, sleepTime):
          super().__init__()
          self.mainWindow = MainWindow
          self.iter = iter
          self.numofIters = numOfIters
+         if sleepTime > 0.3:
+            self.sleepTime = sleepTime
+            self.extendedSleepTime = 1.3 * sleepTime
+         else:
+             self.sleepTime = 0.3
+             self.extendedSleepTime = 0.4
          self.isPaused = False
          self.isRunning = True
 
@@ -18,10 +25,13 @@ class Animation(QRunnable):
                 continue
             self.mainWindow.start_animation()
             self.iter += 1
-            time.sleep(0.3)
+            time.sleep(self.sleepTime)
          self.mainWindow.enableStartButton()
          self.isRunning = False
          print("Thread done")
+
+      def extendSleepTime(self):
+          self.sleepTime = self.extendedSleepTime
 
       def stop(self):
          self.isPaused = True
